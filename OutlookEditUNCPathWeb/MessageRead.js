fd002b4e476f78b825efa54e9346bb9ccd5dd4d4
wmti-bsavage <braedon.savage@webermfg.ca>
2023-07-28 12:38:24 -0400

    Initial commit

---
+++ OutlookEditUNCPathWeb/MessageRead.js
@@ -0,0 +1,20 @@
+﻿Office.onReady((info) => { });
+
+/**
+    * Writes 'Hello world!' to a new message body.
+    */
+function sayHello() {
+    Office.context.mailbox.item.body.setAsync(
+        'Hello world!',
+        {
+            coercionType: 'html', // Write text as HTML
+        },
+
+        // Callback method to check that setAsync succeeded
+        function (asyncResult) {
+            if (asyncResult.status == Office.AsyncResultStatus.Failed) {
+                write(asyncResult.error.message);
+            }
+        }
+    );
+}
